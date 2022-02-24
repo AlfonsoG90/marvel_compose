494fe4f8702934bca5827138f2a637d70786d14d
Alfonso García <alfonsojose.garcia@redsys.es>
2022-02-24 19:27:55 +0100

    Change api parameters

--- app/src/main/java/com/example/marvel/api/MarvelInterceptor.kt
+++ app/src/main/java/com/example/marvel/api/MarvelInterceptor.kt
@@ -1,19 +1,16 @@
 package com.example.marvel.api
 
 import com.example.marvel.BuildConfig
-import com.example.marvel.utils.md5
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class MarvelInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val timestamp = System.currentTimeMillis().toString()
-        val hash = md5(timestamp + BuildConfig.PRIVATE_KEY + BuildConfig.PUBLIC_KEY)
         val original = chain.request()
         val url = original.url.newBuilder()
             .addQueryParameter("apikey",BuildConfig.PUBLIC_KEY)
-            .addQueryParameter("ts",timestamp)
-            .addQueryParameter("hash",hash)
+            .addQueryParameter("ts",BuildConfig.TS)
+            .addQueryParameter("hash",BuildConfig.HASH)
             .build()
 
         val requestBuilder = original.newBuilder().url(url)
